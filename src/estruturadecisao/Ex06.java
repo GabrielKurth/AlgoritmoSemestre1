e11f0fdb6c0e3ddeb6e01b0c4abef8b9bcb24bca
Gabriel <Gabriel@10.1.1.126>
2017-07-24 17:12:51 -0300

    Identificando dia da semana, fim de semana ou dia inválido.

--- src/estruturadecisao/Ex06.java
+++ src/estruturadecisao/Ex06.java
@@ -1,7 +1,7 @@
 /*
 Criar um programa para identificar se um dia da semana (numerados de 1 a 7) é dia de
 semana, fim de semana ou um dia inválido. 
- */
+*/
 package estruturadecisao;
 
 import java.util.Scanner;
